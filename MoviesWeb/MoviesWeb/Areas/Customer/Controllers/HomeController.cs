4f672c97c8aa888e5f16520c682e7d554c487ce6
Promise30 <promiseadeagbo@gmail.com>
2023-06-11 12:19:01 +0100

    Initial commit for Customer Home Controller

---
+++ MoviesWeb/MoviesWeb/Areas/Customer/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MoviesWeb.Data;
+using MoviesWeb.Models;
+using System.Diagnostics;
+
+namespace MoviesWeb.Areas.Customer.Controllers
+{
+    [Area("Customer")]
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext _db;
+
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext db)
+        {
+            _logger = logger;
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Movie> movieList = _db.MovieTable.Include(u => u.Genre).ToList();
+            return View(movieList);
+        }
+
+        public IActionResult Details(int id)
+        {
+            Movie movie = _db.MovieTable.Include(m => m.Genre).First(m => m.Id == id);
+            return View(movie);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
