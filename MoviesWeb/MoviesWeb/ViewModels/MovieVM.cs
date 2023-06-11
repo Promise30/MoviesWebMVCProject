ee55d5cc42fcae89dfe4a8d585bfda60b64761cb
Promise30 <promiseadeagbo@gmail.com>
2023-06-11 12:20:57 +0100

    Initial commit for Movie View Model

---
+++ MoviesWeb/MoviesWeb/ViewModels/MovieVM.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using MoviesWeb.Models;
+
+namespace MoviesWeb.ViewModels
+{
+    public class MovieVM
+    {
+        public Movie Movie { get; set; }
+        [ValidateNever]
+        public IEnumerable<SelectListItem> GenreList { get; set; }
+    }
+}
